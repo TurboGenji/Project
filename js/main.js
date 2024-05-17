63045ab1e7bf4f2d8716e365b5929588fa355265
Alex Tserekhovich(TurboGenji) <talexei2006@gmail.com>
2024-05-17 10:20:05 +0300

    Fixed bug that didn't show button "Learn more"

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ window.onload = function() {
 function animation1() {
     let button1 = document.getElementById ('button1'),
     ad1 = document.getElementById ('ad1'),
-    uns1 =document.getElementById ('uns1'),
     button10 = document.getElementById ('button10'),
     learn = document.getElementById ('learn1');
 
@@ -16,13 +15,11 @@ function animation1() {
     button1.style.backgroundColor = "#6D8EB5"
     ad1.style.backgroundColor = "#6D8EB5";
     button10.style.visibility = "hidden";
-    uns1.style.opacity = "1";
     learn.style.opacity = "1";
 }
 function animation10() {
     let button1 = document.getElementById ('button1'),
     ad1 = document.getElementById ('ad1'),
-    uns1 =document.getElementById ('uns1'),
     button10 = document.getElementById ('button10'),
     learn = document.getElementById ('learn1');
 
@@ -30,13 +27,11 @@ function animation10() {
     button1.style.backgroundColor = "#dfe3ed";
     ad1.style.backgroundColor = "#dfe3ed";
     button10.style.visibility = "visible";
-    uns1.style.opacity = "0";
     learn.style.opacity = "0";
 }
 function animation2() {
     let button2 = document.getElementById ('button2'),
     ad2 = document.getElementById ('ad2'),
-    uns2 =document.getElementById ('uns2'),
     button20 = document.getElementById ('button20'),
     learn = document.getElementById ('learn2');
 
@@ -45,13 +40,11 @@ function animation2() {
     button2.style.backgroundColor = "#6D8EB5"
     ad2.style.backgroundColor = "#6D8EB5";
     button20.style.visibility = "hidden";
-    uns2.style.opacity = "1";
     learn.style.opacity = "1";
 }
 function animation20() {
     let button2 = document.getElementById ('button2'),
     ad2 = document.getElementById ('ad2'),
-    uns2 =document.getElementById ('uns2'),
     button20 = document.getElementById ('button20'),
     learn= document.getElementById ('learn2');
 
@@ -59,13 +52,11 @@ function animation20() {
     button2.style.backgroundColor = "#dfe3ed";
     ad2.style.backgroundColor = "#dfe3ed";
     button20.style.visibility = "visible";
-    uns2.style.opacity = "0";
     learn.style.opacity = "0";
 }
 function animation3() {
     let button3 = document.getElementById ('button3'),
     ad3 = document.getElementById ('ad3'),
-    uns3 =document.getElementById ('uns3'),
     button30 = document.getElementById ('button30');
     learn= document.getElementById ('learn3');
 
@@ -74,13 +65,11 @@ function animation3() {
     button3.style.backgroundColor = "#6D8EB5"
     ad3.style.backgroundColor = "#6D8EB5";
     button30.style.visibility = "hidden";
-    uns3.style.opacity = "1";
     learn.style.opacity = "1";
 }
 function animation30() {
     let button3 = document.getElementById ('button3'),
     ad3 = document.getElementById ('ad3'),
-    uns3 =document.getElementById ('uns3'),
     button30 = document.getElementById ('button30');
     learn= document.getElementById ('learn3');
 
@@ -88,6 +77,5 @@ function animation30() {
     button3.style.backgroundColor = "#dfe3ed";
     ad3.style.backgroundColor = "#dfe3ed";
     button30.style.visibility = "visible";
-    uns3.style.opacity = "0";
     learn.style.opacity = "0";
 }
